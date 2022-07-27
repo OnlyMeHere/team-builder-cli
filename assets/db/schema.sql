d1ef7776b778b1a0fb8053ff2f1c98cb894e5b6e
OnlyMeHere <bennett45.j.m@gmail.com>
2022-07-27 16:38:40 -0700

    add data tables

--- assets/db/schema.sql
+++ assets/db/schema.sql
@@ -2,3 +2,22 @@ DROP DATABASE IF EXISTS myCompany(db);
 CREATE DATABASE myCompany(db);
 USE myCompany;
 
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(5,2),
+    department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id int
+);
